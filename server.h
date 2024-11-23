33998aba2f029d7a06e7fdb9c885ee9027283514
zrefalogia <vuongquocphongv@gmail.com>
2024-11-23 23:45:42 +0700

    reformat code

--- server.h
+++ server.h
@@ -1,28 +1,29 @@
 #pragma once
-#include <iostream>
-#include <thread>
 #include <arpa/inet.h>
-#include <unordered_map>
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <atomic>
 #include <cstring>
 #include <fcntl.h>
+#include <iostream>
 #include <mutex>
-#include <atomic>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
+#include <unordered_map>
 
 const int MAX_EVENTS = 10;
 const int BUFFER_SIZE = 1024;
 
-class Server {
-public:
+class Server
+{
+  public:
     Server(int port);
     ~Server();
     void start();
     void stop();
 
-private:
+  private:
     int server_fd;
     int port;
     std::unordered_map<int, std::string> clients;
@@ -33,5 +34,5 @@ private:
     void make_socket_non_blocking(int socket_fd);
     void command_handler();
     void client_handler();
-    void reply_to_client(int client_fd, const std::string& message);
+    void reply_to_client(int client_fd, const std::string &message);
 };
